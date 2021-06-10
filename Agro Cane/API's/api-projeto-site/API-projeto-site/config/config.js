0f1a8b4cbb3f6b3877f363cab822109279dde78c
Mateusvl2003 <mateus.ferreira3103@gmail.com>
2021-06-10 11:56:27 -0300

    Adicionei o modelo de documentação

--- Agro Cane/API's/api-projeto-site/API-projeto-site/config/config.js
+++ Agro Cane/API's/api-projeto-site/API-projeto-site/config/config.js
@@ -1,47 +1,47 @@
 module.exports = {
-  // Insira aqui seus dados do banco NA NUVEM AZURE
-  production: {
-    // altere APENAS username, password, database e host.
-    username: 'bandtec',
-    password: '#Gfgrupo9',
-    database: '211-1a-grupo9',
-    host: '2111agrupo9.database.windows.net',
-    dialect: 'mssql',
-    xuse_env_variable: 'DATABASE_URL',
-    dialectOptions: {
-      options: {
-        encrypt: true
-      }
+    // Insira aqui seus dados do banco NA NUVEM AZURE
+    production: {
+        // altere APENAS username, password, database e host.
+        username: 'bandtec',
+        password: '#Gfgrupo9',
+        database: '211-1a-grupo9',
+        host: '2111agrupo9.database.windows.net',
+        dialect: 'mssql',
+        xuse_env_variable: 'DATABASE_URL',
+        dialectOptions: {
+            options: {
+                encrypt: true
+            }
+        },
+        pool: {
+            max: 5,
+            min: 1,
+            acquire: 5000,
+            idle: 30000,
+            connectTimeout: 5000
+        }
     },
-    pool: { 
-      max: 5,
-      min: 1,
-      acquire: 5000,
-      idle: 30000,
-      connectTimeout: 5000
-    }
-  },
 
-  // Insira aqui seus dados do banco LOCAL - MySQL Workbench
-  dev: {
-    // altere APENAS username, password e database.
-    username: 'apaga_hoje',
-    password: '001122',
-    database: 'agroCane',
-    host: 'localhost',
-    dialect: 'mysql',
-    xuse_env_variable: 'DATABASE_URL',
-    dialectOptions: {
-      options: {
-        encrypt: true
-      }
+    // Insira aqui seus dados do banco LOCAL - MySQL Workbench
+    dev: {
+        // altere APENAS username, password e database.
+        username: 'trabalhogrupo',
+        password: 'urubu100',
+        database: 'TrabalhoAgro',
+        host: 'localhost',
+        dialect: 'mysql',
+        xuse_env_variable: 'DATABASE_URL',
+        dialectOptions: {
+            options: {
+                encrypt: true
+            }
+        },
+        pool: {
+            max: 5,
+            min: 1,
+            acquire: 5000,
+            idle: 30000,
+            connectTimeout: 5000
+        }
     },
-    pool: { 
-      max: 5,
-      min: 1,
-      acquire: 5000,
-      idle: 30000,
-      connectTimeout: 5000
-    }
-  },
 };
