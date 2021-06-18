93efcc477de2b20409b9990cb6d7826cdcef95fb
thais-camoliveira <thais.oliveira@bandtec.com.br>
2021-06-18 02:30:25 -0300

    ultimo

--- Agro Cane/API's/api-projeto-site/API-projeto-site/routes/leituras.js
+++ Agro Cane/API's/api-projeto-site/API-projeto-site/routes/leituras.js
@@ -5,65 +5,65 @@ var Leitura = require('../models').dadoSensor;
 var env = process.env.NODE_ENV || 'development';
 
 /* Recuperar as últimas N leituras */
-router.get('/ultimas/:idcaminhao', function (req, res, next) {
+// router.get('/ultimas/:idcaminhao', function (req, res, next) {
 
-	// quantas são as últimas leituras que quer? 7 está bom?
+// 	// quantas são as últimas leituras que quer? 7 está bom?
 
-	const limite_linhas = 7;
+// 	const limite_linhas = 7;
 
-	var idcaminhao = req.params.idcaminhao;
+// 	var idcaminhao = req.params.idcaminhao;
 
-	console.log(`Recuperando as ultimas ${limite_linhas} leituras`);
+// 	console.log(`Recuperando as ultimas ${limite_linhas} leituras`);
 
-	let instrucaoSql = "";
-	// let instrucaoSql2 = "";
+// 	let instrucaoSql = "";
+// 	// let instrucaoSql2 = "";
 
-	if (env == 'dev') {
-		// abaixo, escreva o select de dados para o Workbench
-		instrucaoSql = `select 
-		temperatura, 
-		umidade, 
-		momento,
-		DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-		from leitura
-		where fkcaminhao = ${idcaminhao}
-		order by id desc limit ${limite_linhas}`;
+// 	if (env == 'dev') {
+// 		// abaixo, escreva o select de dados para o Workbench
+// 		instrucaoSql = `select 
+// 		temperatura, 
+// 		umidade, 
+// 		momento,
+// 		DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
+// 		from leitura
+// 		where fkcaminhao = ${idcaminhao}
+// 		order by id desc limit ${limite_linhas}`;
 
-		// instrucaoSql2 = `select 
-		// temperatura, 
-		// umidade, 
-		// momento,
-		// DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-		// from leitura
-		// where fkcaminhao = ${idcaminhao}
-		// order by id desc limit ${limite_linhas}`;
+// 		// instrucaoSql2 = `select 
+// 		// temperatura, 
+// 		// umidade, 
+// 		// momento,
+// 		// DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
+// 		// from leitura
+// 		// where fkcaminhao = ${idcaminhao}
+// 		// order by id desc limit ${limite_linhas}`;
 
-	} else if (env == 'production') {
-		// abaixo, escreva o select de dados para o SQL Server
-		instrucaoSql = `select top ${limite_linhas} 
-		temperatura, 
-		umidade, 
-		momento,
-		FORMAT(momento,'HH:mm:ss') as momento_grafico
-		from leitura
-		where fkcaminhao = ${idcaminhao}
-		order by id desc`;
-	} else {
-		console.log("\n\n\n\nproduction\n\n\n\n")
-	}
+// 	} else if (env == 'production') {
+// 		// abaixo, escreva o select de dados para o SQL Server
+// 		instrucaoSql = `select top ${limite_linhas} 
+// 		temperatura, 
+// 		umidade, 
+// 		momento,
+// 		FORMAT(momento,'HH:mm:ss') as momento_grafico
+// 		from leitura
+// 		where fkcaminhao = ${idcaminhao}
+// 		order by id desc`;
+// 	} else {
+// 		console.log("\n\n\n\nproduction\n\n\n\n")
+// 	}
 
-	sequelize.query(instrucaoSql, {
-		model: Leitura,
-		mapToModel: true
-	})
-		.then(resultado => {
-			console.log(`Encontrados: ${resultado.length}`);
-			res.json(resultado);
-		}).catch(erro => {
-			console.error(erro);
-			res.status(500).send(erro.message);
-		});
-});
+// 	sequelize.query(instrucaoSql, {
+// 		model: Leitura,
+// 		mapToModel: true
+// 	})
+// 		.then(resultado => {
+// 			console.log(`Encontrados: ${resultado.length}`);
+// 			res.json(resultado);
+// 		}).catch(erro => {
+// 			console.error(erro);
+// 			res.status(500).send(erro.message);
+// 		});
+// });
 
 
 router.get('/tempo-real/:sensores_diversos', function (req, res, next) {
@@ -95,30 +95,30 @@ router.get('/tempo-real/:sensores_diversos', function (req, res, next) {
 		});
 });
 
-// estatísticas (max, min, média, mediana, quartis etc)
-router.get('/estatisticas', function (req, res, next) {
+// // estatísticas (max, min, média, mediana, quartis etc)
+// router.get('/estatisticas', function (req, res, next) {
 
-	console.log(`Recuperando as estatísticas atuais`);
+// 	console.log(`Recuperando as estatísticas atuais`);
 
-	const instrucaoSql = `select 
-							max(temperatura) as temp_maxima, 
-							min(temperatura) as temp_minima, 
-							avg(temperatura) as temp_media,
-							max(umidade) as umidade_maxima, 
-							min(umidade) as umidade_minima, 
-							avg(umidade) as umidade_media 
-						from leitura`;
+// 	const instrucaoSql = `select 
+// 							max(temperatura) as temp_maxima, 
+// 							min(temperatura) as temp_minima, 
+// 							avg(temperatura) as temp_media,
+// 							max(umidade) as umidade_maxima, 
+// 							min(umidade) as umidade_minima, 
+// 							avg(umidade) as umidade_media 
+// 						from leitura`;
 
 
-	sequelize.query(instrucaoSql, { type: sequelize.QueryTypes.SELECT })
-		.then(resultado => {
-			res.json(resultado[0]);
-		}).catch(erro => {
-			console.error(erro);
-			res.status(500).send(erro.message);
-		});
+// 	sequelize.query(instrucaoSql, { type: sequelize.QueryTypes.SELECT })
+// 		.then(resultado => {
+// 			res.json(resultado[0]);
+// 		}).catch(erro => {
+// 			console.error(erro);
+// 			res.status(500).send(erro.message);
+// 		});
 
-});
+// });
 
 // GRAFICO ESTATICO PIZZA -- DASHBOARD INICIAL
 
